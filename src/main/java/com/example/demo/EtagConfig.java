94a3106b9ddf0ee6ad9697e10117bf1a8b258a92
David Buckell <davidbuckell1978@gmail.com>
2025-11-16 23:43:39 +0000

    Simplified EtagConfig

--- src/main/java/com/example/demo/EtagConfig.java
+++ src/main/java/com/example/demo/EtagConfig.java
@@ -1,20 +1,16 @@
 package com.example.demo;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import jakarta.servlet.Filter;
 
 @Configuration
 public class EtagConfig {
 
     @Bean
-    public FilterRegistrationBean<ShallowEtagHeaderFilter> etagFilter() {
-        FilterRegistrationBean<ShallowEtagHeaderFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new ShallowEtagHeaderFilter());
-        registration.addUrlPatterns("/items/*"); // or "/*" for all
-        registration.setName("etagFilter");
-        registration.setOrder(1);
-        return registration;
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
